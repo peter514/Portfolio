a7afab82d2df62fd036b5307e3d15592a8aa3990
peter514 <peterkgachora1@gmail.com>
2023-12-03 21:46:20 +0300

    updates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
         tertiary: "#000"
       },
       backgroundImage: {
-        heroBg: "url('https://i.ibb.co/0QWTkBp/herob.jpg')"
+        heroBg: "url('/src/assets/images/hero.jpg')"
       }
     },
   },
